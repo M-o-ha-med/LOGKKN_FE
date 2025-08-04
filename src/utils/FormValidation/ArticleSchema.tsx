ebbb96ec32f6a9e4c0be6a9ac3020905612d2abe
M-o-ha-med <120997421+M-o-ha-med@users.noreply.github.com>
2025-08-04 21:53:57 +0700

    Update ArticleSchema.tsx

--- src/utils/FormValidation/ArticleSchema.tsx
+++ src/utils/FormValidation/ArticleSchema.tsx
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const createArticleSchema = z.object({
   title: z.string().min(5, 'Title must be at least 5 characters'),
   content: z.string().min(10, 'Content must be at least 10 characters'),
-  images: z.custom<File>((file) => file instanceof File, 'Thumbnail is required')
+  images: z.array(z.instanceof(File)).min(1 , 'images are required'),
 });
 
 export const updateArticleSchema = z.object({
